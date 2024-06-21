87d56493b6ce1047611c0c91053281254fa2271d
lucasolopes <olopes.lucas567@gmail.com>
2024-06-21 11:00:11 -0300

    Inicializando

---
+++ src/main/java/com/desafio/picpay/PicPaySimplifica/PicPaySimplificaApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.picpay.PicPaySimplifica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPaySimplificaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPaySimplificaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/picpay/PicPaySimplifica/PicPaySimplificaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.picpay.PicPaySimplifica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicPaySimplificaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
