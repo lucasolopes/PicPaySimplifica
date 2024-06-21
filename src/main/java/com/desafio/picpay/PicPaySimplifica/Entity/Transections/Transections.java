2a53572d96407a492f56976bba2d7c2a4d8ee04b
lucasolopes <olopes.lucas567@gmail.com>
2024-06-21 14:20:52 -0300

    criando entidade transection

---
+++ src/main/java/com/desafio/picpay/PicPaySimplifica/Entity/Transections/Transections.java
@@ -0,0 +1,29 @@
+package com.desafio.picpay.PicPaySimplifica.Entity.Transections;
+
+import com.desafio.picpay.PicPaySimplifica.Entity.User.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "Transections")
+@Table(name = "Transections")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transections {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne
+    private User sender;
+
+    @ManyToOne
+    private User Reciver;
+
+    private BigDecimal value;
+}
