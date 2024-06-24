9d3e660d8681f748400a1d0711a2d874cb6a2fe9
lucasolopes <olopes.lucas567@gmail.com>
2024-06-24 04:34:32 -0300

    criando query de busca usuario por Id

--- src/main/java/com/desafio/picpay/PicPaySimplifica/Repository/UserRepository.java
+++ src/main/java/com/desafio/picpay/PicPaySimplifica/Repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.desafio.picpay.PicPaySimplifica.Repository;
 
+import com.desafio.picpay.PicPaySimplifica.Entity.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.desafio.picpay.PicPaySimplifica.Entity.User.User;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findUserById(Long id);
 }
