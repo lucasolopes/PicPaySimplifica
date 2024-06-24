29cd949c3a517983e20e33f00ecdae26bb3a62ea
lucasolopes <olopes.lucas567@gmail.com>
2024-06-24 04:35:18 -0300

    buscando usuario e validando se usuario é valido para transacao na service

--- src/main/java/com/desafio/picpay/PicPaySimplifica/Service/UserService.java
+++ src/main/java/com/desafio/picpay/PicPaySimplifica/Service/UserService.java
@@ -2,9 +2,12 @@ package com.desafio.picpay.PicPaySimplifica.Service;
 
 import com.desafio.picpay.PicPaySimplifica.Dtos.UserDto;
 import com.desafio.picpay.PicPaySimplifica.Entity.User.User;
+import com.desafio.picpay.PicPaySimplifica.Entity.User.UserType;
 import com.desafio.picpay.PicPaySimplifica.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class UserService {
@@ -22,4 +25,16 @@ public class UserService {
         this.userRepository.save(user);
     }
 
+    public User findById(Long id) throws Exception {
+        return this.userRepository.findUserById(id).orElseThrow(() -> new Exception("User nao encontrado"));
+    }
+
+    public void validatedTransection(User sender, BigDecimal amount) throws Exception {
+        if (sender.getType() == UserType.Logista) {
+            throw new Exception("Usuario é do tipo Logista. Nao esta autorizado a realizar tranzacao");
+        }
+        if (sender.getSaldo().compareTo(amount) < 0) {
+            throw new Exception("saldo insuficiente");
+        }
+    }
 }
