6dbdb9af7d9db197d3bd1d7442ac3e56c46e31b4
lucasolopes <olopes.lucas567@gmail.com>
2024-06-24 04:36:38 -0300

    criando Service de Transections e realiando transferia

---
+++ src/main/java/com/desafio/picpay/PicPaySimplifica/Service/TransectionsService.java
@@ -0,0 +1,63 @@
+package com.desafio.picpay.PicPaySimplifica.Service;
+
+import com.desafio.picpay.PicPaySimplifica.Dtos.TransectionsDto;
+import com.desafio.picpay.PicPaySimplifica.Entity.Transections.Transections;
+import com.desafio.picpay.PicPaySimplifica.Entity.User.User;
+import com.desafio.picpay.PicPaySimplifica.Repository.TransectionsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+
+@Service
+public class TransectionsService {
+
+    @Autowired
+    private TransectionsRepository transectionsRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sender(TransectionsDto transection) throws Exception {
+        User sender = this.userService.findById(transection.getSenderId());
+        User reciver = this.userService.findById(transection.getReceiverId());
+
+        userService.validatedTransection(sender, transection.getValue());
+
+        if (!(this.authorizeTransaction(sender, transection.getValue()))) {
+            throw new Exception("Transacao nao autorizada");
+        }
+
+        Transections newTransection = new Transections();
+        newTransection.setAmount(transection.getValue());
+        newTransection.setSender(sender);
+        newTransection.setReciver(reciver);
+        newTransection.setTimeStamp(LocalDateTime.now());
+
+        sender.setSaldo(sender.getSaldo().subtract(transection.getValue()));
+        reciver.setSaldo(reciver.getSaldo().add(transection.getValue()));
+
+        this.transectionsRepository.save(newTransection);
+        this.userService.saveUser(sender);
+        this.userService.saveUser(reciver);
+    }
+
+    public boolean authorizeTransaction(User sender, BigDecimal value) {
+        ResponseEntity<Map> autorizeResponse = restTemplate.getForEntity("https://util.devi.tools/api/v2/authorize", Map.class);
+        if (autorizeResponse.getStatusCode() == HttpStatus.OK) {
+            String message = (String) autorizeResponse.getBody().get("message");
+            return "Autorizado".equalsIgnoreCase(message);
+        } else return false;
+    }
+
+
+}
